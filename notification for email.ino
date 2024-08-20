c61ce5381eb890b4d86caaf69aee1a78cb12ac63
Steve Sequeira <115017634+Steveseq@users.noreply.github.com>
2024-08-21 00:54:23 +0530

    EmailNotification.ino

---
+++ notification for email.ino
@@ -0,0 +1,34 @@
+
+ //notification for email
+ int led =13;            // led is connected to esp32 D13
+ int pirdata =15 ;       // pir D is connected to D15
+ int pirstate =LOW ;      // assuming no motion 
+ int value =0;            // to read pin status 
+
+void setup() {
+  pinMode(led, OUTPUT);      //  Led as output
+  pinMode(pirdata, INPUT);     // PIR sensor as input
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  value = digitalRead(pirdata);  
+  if (value == HIGH) {           
+    digitalWrite(led, HIGH);  
+    if (pirstate == LOW) {
+     
+      Serial.println("Mail received!");
+      
+      pirstate = HIGH;
+    }
+  } else {
+    digitalWrite(led, LOW); 
+    if (pirstate == HIGH) {
+      
+      Serial.println("Mail detected please collect it.");
+     
+      pirstate = LOW;
+    }
+  }
+}
